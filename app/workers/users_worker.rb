874cbf812f4a8bc7efcc39efbc747fba392872dc
hizqeelrz <hizqeel1214@gmail.com>
2016-09-16 21:37:18 +0500

    bug solved in application controller auth error

--- app/workers/users_worker.rb
+++ app/workers/users_worker.rb
@@ -6,3 +6,11 @@ class UsersWorker
     UserMailer.signup_confirmation(@user).deliver_now	
 	end
 end
+
+# app directory
+
+# bundle exec sidekiq --environment development -C config/sidekiq.yml
+
+# redis-server # root directory
+
+# rails s as we know
